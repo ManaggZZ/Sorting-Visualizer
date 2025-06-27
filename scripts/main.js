16249afed92c6f4e9cf316288c3f7693607c1cc9
Manan Aggarwal <126272962+ManaggZZ@users.noreply.github.com>
2025-06-27 21:27:35 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-//Variables (BE CAREFUL THESE MIGHT BE USED IN OTHER JS FILES TOO)
+//Variables
 var inp_as=document.getElementById('a_size'),array_size=inp_as.value;
 var inp_gen=document.getElementById("a_generate");
 var inp_aspeed=document.getElementById("a_speed");
